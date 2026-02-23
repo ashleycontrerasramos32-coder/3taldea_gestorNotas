673d224854c229ea4248bf83f45283a687280be2
Ashley Contreras <ashleycontrerasramos32@gmail.com>
2026-02-23 13:33:03 +0100

    calcularPromedio metodoan suma zati contador egin behar da.

--- gestorNotas/src/gestor/GestorNotas.java
+++ gestorNotas/src/gestor/GestorNotas.java
@@ -20,7 +20,7 @@ public class GestorNotas {
         for (int i = 0; i < notas.length; i++) {
             suma += notas[i];
         }
-        return suma / notas.length;
+        return suma / contador;
     }
 
     public double obtenerNotaMaxima() {
